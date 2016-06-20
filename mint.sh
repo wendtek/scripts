96717f26627ae99a5437e76d3b8997c9d33f9e9d
Greg Palmier <gregpalmier@gmail.com>
2016-06-20 09:31:55 -0500

    pidgin

--- mint.sh
+++ mint.sh
@@ -3,7 +3,7 @@ apt-get update && apt-get -y upgrade
 apt-get -y install vim wget irssi rbenv ruby-build mutt tmux pwgen elinks silversearcher-ag sysv-rc-conf golang openssh-server
 apt-get -y install gimp firefox libreoffice php5 gedit vlc wireshark zsh tightvncserver ntp python-pip fail2ban remmina
 apt-get -y install curl guake nmap libssl-dev lynx chromium-browser libappindicator-dev nodejs libsqlite3-dev elinks traceroute
-apt-get -y install dropbox bless inkscape
+apt-get -y install dropbox bless inkscape pidgin
 
 #chrome
 wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb

b8fe0493439bd9e39fb202e356920983614ab0bf
Greg Palmier <gregpalmier@gmail.com>
2016-06-23 08:49:54 -0500

    stuff

--- mint.sh
+++ mint.sh
@@ -5,6 +5,11 @@ apt-get -y install gimp firefox libreoffice php5 gedit vlc wireshark zsh tightvn
 apt-get -y install curl guake nmap libssl-dev lynx libappindicator-dev nodejs libsqlite3-dev traceroute
 apt-get -y install dropbox bless inkscape pidgin
 
+apt-get -y install libpq-dev libpcap-dev
+
+# heroku toolbelt
+wget -O- https://toolbelt.heroku.com/install-ubuntu.sh | sh
+
 #chrome
 wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
 dpkg -i google-chrome-stable_current_amd64.deb

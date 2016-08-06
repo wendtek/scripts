b607ec583a6b5cd002c835f0fb61206678dc4a9c
Greg Palmier <gregpalmier@gmail.com>
2016-08-06 16:46:12 -0500

    join rancher

--- ufw.sh
+++ ufw.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 apt-get install ufw
 ufw logging on
-ufw allow 22/tcp
+ufw allow proto tcp from 192.168.1.0/24 to any port 22
+ufw allow proto udp from 192.168.1.0/24 to any port 500
+ufw allow proto udp from 192.168.1.0/24 to any port 4500
 ufw enable

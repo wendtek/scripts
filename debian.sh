8cd37485de17111e32ebdbd1a9ea5bd6cd479b06
Greg Palmier <gregpalmier@gmail.com>
2016-08-20 23:01:02 -0500

    httpie

--- debian.sh
+++ debian.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 apt-get update && aptitude -y upgrade
-apt-get -y install vim wget mutt tmux pwgen elinks silversearcher-ag sysv-rc-conf openssh-server
-apt-get -y install zsh ntp python-pip aptitude
-apt-get -y install curl nmap libssl-dev lynx libappindicator-dev nodejs libsqlite3-dev traceroute
+apt-get -y install vim wget mutt tmux pwgen elinks silversearcher-ag \
+sysv-rc-conf openssh-server httpie zsh ntp python-pip aptitude \
+curl nmap libssl-dev lynx libappindicator-dev nodejs libsqlite3-dev traceroute
--- mint.sh
+++ mint.sh
@@ -1,11 +1,12 @@
 #!/bin/sh
 apt-get update && apt-get -y upgrade
-apt-get -y install aptitude vim wget irssi rbenv ruby-build mutt tmux pwgen elinks silversearcher-ag sysv-rc-conf golang openssh-server
-apt-get -y install gimp firefox libreoffice gedit vlc wireshark zsh tightvncserver ntp python-pip fail2ban remmina
-apt-get -y install curl guake nmap libssl-dev lynx libappindicator-dev nodejs libsqlite3-dev traceroute libcurl3
-apt-get -y install dropbox bless inkscape pidgin docker.io
-
-apt-get -y install libpq-dev libpcap-dev
+apt-get -y install aptitude vim wget irssi rbenv ruby-build mutt tmux \
+pwgen elinks silversearcher-ag sysv-rc-conf golang openssh-server \
+gimp firefox libreoffice gedit vlc wireshark zsh \
+tightvncserver ntp python-pip fail2ban remmina httpie \
+curl guake nmap libssl-dev lynx libappindicator-dev nodejs \
+libsqlite3-dev traceroute libcurl3 dropbox bless inkscape pidgin \
+docker.io libpq-dev libpcap-dev httpie libffi-dev
 
 # heroku toolbelt
 wget -O- https://toolbelt.heroku.com/install-ubuntu.sh | sh
@@ -15,5 +16,4 @@ wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
 dpkg -i google-chrome-stable_current_amd64.deb
 rm google-chrome-stable_current_amd64.deb
 
-apt-get -y install libffi-dev
 sudo pip install awscli powerline-status cryptography

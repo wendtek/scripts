cd2aa36d43ee274f2507b853c1c95799a67a5134
Greg Palmier <gregpalmier@gmail.com>
2016-08-25 09:37:43 -0500

    ufw

--- jenkins.sh
+++ jenkins.sh
@@ -1,5 +1,10 @@
-#!/bin/sh
+#!/bin/bash
 wget -q -O - http://pkg.jenkins-ci.org/debian/jenkins-ci.org.key | sudo apt-key add -
 echo "deb http://pkg.jenkins-ci.org/debian binary/" > /etc/apt/sources.list.d/jenkins.list
 apt-get update
-apt-get install -y jenkins
+apt-get install -y jenkins ufw
+
+ufw logging on
+ufw allow 8080/tcp
+ufw allow from 192.168.1.0/24 to any port 22 proto tcp
+ufw enable

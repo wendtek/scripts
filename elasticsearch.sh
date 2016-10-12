549b783695c3aaf29747072980db587335ac51e1
Greg Palmier <gregpalmier@gmail.com>
2016-10-12 18:48:58 -0500

    kibana

--- elasticsearch.sh
+++ elasticsearch.sh
@@ -3,6 +3,9 @@ apt-get update
 wget https://download.elastic.co/elasticsearch/release/org/elasticsearch/distribution/deb/elasticsearch/2.4.1/elasticsearch-2.4.1.deb
 dpkg -i elasticsearch-2.4.1.deb
 rm elasticsearch-2.4.1.deb
+wget https://download.elastic.co/kibana/kibana/kibana-4.6.1-amd64.deb
+dpkg -i kibana-4.6.1-amd64.deb
+rm kibana-4.6.1-amd64.deb
 
 echo 'configure /etc/elasticsearch/elasticsearch.yml to listen on 9200'
 

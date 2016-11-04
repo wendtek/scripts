60560a48692fff2eb4bf6f2857569cceb32f7d96
Greg Palmier <gregpalmier@gmail.com>
2016-11-04 13:23:29 -0500

    tcpdump

--- debian.sh
+++ debian.sh
@@ -2,4 +2,5 @@
 apt-get update && aptitude -y upgrade
 apt-get -y install vim wget mutt tmux pwgen elinks silversearcher-ag \
 sysv-rc-conf openssh-server httpie zsh ntp python-pip aptitude \
-curl nmap libssl-dev lynx libappindicator-dev nodejs libsqlite3-dev traceroute
+curl nmap libssl-dev lynx libappindicator-dev nodejs libsqlite3-dev traceroute \
+tshark tcpdump
